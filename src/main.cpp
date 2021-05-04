d8efcb140b7cf9db1adb2bc2686e918d7a6baaae
yumcyawiz <hashdolf1923@gmail.com>
2021-05-04 14:51:47 +0900

    read dragon.obj with tinyobjloader

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,66 @@
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <memory>
+#include <string>
+
 #include "aabb.hpp"
+#include "polygon.hpp"
+#include "tiny_obj_loader.h"
 #include "vec3.hpp"
 
-int main() { return 0; }
+bool loadObj(const std::string& filename, std::vector<float>& vertices,
+             std::vector<unsigned int>& indices, std::vector<float>& normals,
+             std::vector<float>& uvs) {
+  tinyobj::ObjReader reader;
+
+  if (!reader.ParseFromFile(filename)) {
+    if (!reader.Error().empty()) {
+      std::cerr << reader.Error();
+    }
+    return false;
+  }
+
+  if (!reader.Warning().empty()) {
+    std::cout << reader.Warning();
+  }
+
+  const auto& attrib = reader.GetAttrib();
+  const auto& shapes = reader.GetShapes();
+  const auto& materials = reader.GetMaterials();
+
+  vertices = attrib.vertices;
+  if (attrib.normals.size() == attrib.vertices.size()) {
+    normals = attrib.normals;
+  }
+  if (attrib.texcoords.size() == (attrib.vertices.size() / 3) * 2) {
+    uvs = attrib.texcoords;
+  }
+
+  for (size_t s = 0; s < shapes.size(); ++s) {
+    for (const auto& idx : shapes[s].mesh.indices) {
+      indices.push_back(idx.vertex_index);
+    }
+  }
+
+  return true;
+}
+
+int main() {
+  std::string filename = "dragon.obj";
+
+  std::vector<float> vertices;
+  std::vector<unsigned int> indices;
+  std::vector<float> normals;
+  std::vector<float> uvs;
+
+  if (!loadObj(filename, vertices, indices, normals, uvs)) {
+    std::exit(EXIT_FAILURE);
+  }
+
+  const auto polygon = std::make_shared<Polygon>(
+      indices.size(), vertices.data(), indices.data());
+
+  std::cout << "vertices: " << polygon->nVertices << std::endl;
+  std::cout << "faces: " << polygon->nFaces() << std::endl;
+
+  return 0;
+}
