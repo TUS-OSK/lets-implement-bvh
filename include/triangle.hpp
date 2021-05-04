24838463ba7eabca0950d5fc9a6c5618d80b2489
yumcyawiz <hashdolf1923@gmail.com>
2021-05-04 18:05:09 +0900

    implement triangle intersect

--- include/triangle.hpp
+++ include/triangle.hpp
@@ -28,7 +28,40 @@ class Triangle {
     return AABB(pMin, pMax);
   }
 
-  bool intersect(const Ray& ray, IntersectInfo& info) const { return false; }
+  bool intersect(const Ray& ray, IntersectInfo& info) const {
+    const auto indices = polygon->getIndices(faceID);
+    const Vec3 v1 = polygon->getVertex(indices[0]);
+    const Vec3 v2 = polygon->getVertex(indices[1]);
+    const Vec3 v3 = polygon->getVertex(indices[2]);
+
+    // https://www.tandfonline.com/doi/abs/10.1080/10867651.1997.10487468
+    constexpr float EPS = 1e-8;
+    const Vec3 e1 = v2 - v1;
+    const Vec3 e2 = v3 - v1;
+
+    const Vec3 pvec = cross(ray.direction, e2);
+    const float det = dot(e1, pvec);
+
+    if (det > -EPS && det < EPS) return false;
+    const float invDet = 1.0f / det;
+
+    const Vec3 tvec = ray.origin - v1;
+    const float u = dot(tvec, pvec) * invDet;
+    if (u < 0.0f || u > 1.0f) return false;
+
+    const Vec3 qvec = tvec - v1;
+    const float v = dot(ray.direction, qvec) * invDet;
+    if (v < 0.0f || u + v > 1.0f) return false;
+
+    const float t = dot(e2, qvec) * invDet;
+
+    info.t = t;
+    info.hitPos = ray(t);
+    info.barycentric[0] = u;
+    info.barycentric[1] = v;
+
+    return true;
+  }
 };
 
 #endif
