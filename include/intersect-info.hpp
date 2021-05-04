5bc43378db95928171ff11d93d88000089454173
yumcyawiz <hashdolf1923@gmail.com>
2021-05-04 12:57:37 +0900

    add intersect info

---
+++ include/intersect-info.hpp
@@ -0,0 +1,15 @@
+#ifndef _INTERSECT_INFO_H
+#define _INTERSECT_INFO_H
+#include "vec3.hpp"
+
+struct IntersectInfo {
+  float t;
+  Vec3 hitPos;
+  Vec3 hitNormal;
+  float barycentric[2];
+  float uv[2];
+  int geomID;
+  int primID;
+};
+
+#endif
